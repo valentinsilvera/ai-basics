0945baadd6726308973915fc41b2ea8e2cb85627
Valentin <valentin.silvera@code.berlin>
2022-05-02 10:51:27 +0200

    added SnacksVision app

---
+++ CreateML/SnacksVision/SnacksVision/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  SnacksVision
+//
+//  Created by Valentin Silvera on 02.04.22.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ CreateML/SnacksVision/SnacksVision/CGImagePropertyOrientation.swift
@@ -0,0 +1,34 @@
+//
+//  CGImagePropertyOrientation.swift
+//  SnacksVision
+//
+//  Created by Valentin Silvera on 02.04.22.
+//
+
+import UIKit
+
+extension CGImagePropertyOrientation {
+    init(_ orientation: UIImage.Orientation) {
+        switch orientation {
+        case .upMirrored: self = .upMirrored
+        case .down: self = .down
+        case .downMirrored: self = .downMirrored
+        case .left: self = .left
+        case .leftMirrored: self = .leftMirrored
+        case .right: self = .right
+        case .rightMirrored: self = .rightMirrored
+        default: self = .up
+        }
+    }
+}
+
+extension CGImagePropertyOrientation {
+    init(_ orientation: UIDeviceOrientation) {
+        switch orientation {
+        case .portraitUpsideDown: self = .left
+        case .landscapeLeft: self = .up
+        case .landscapeRight: self = .down
+        default: self = .right
+        }
+    }
+}
